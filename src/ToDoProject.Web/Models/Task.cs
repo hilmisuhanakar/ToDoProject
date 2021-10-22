cd28504f86a08cc8217c69e3b05ced9c48cad788
Hilmi Sühan Akar <hilmisuhanakar@outlook.com.tr>
2021-10-22 22:42:57 +0300

    Translated into Turkish.
    The feature names in the design have been translated into Turkish.

--- src/ToDoProject.Web/Models/Task.cs
+++ src/ToDoProject.Web/Models/Task.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 
 #nullable disable
 
@@ -8,11 +9,20 @@ namespace ToDoProject.Web.Models
     public partial class Task
     {
         public int Id { get; set; }
+
+        [DisplayName("Kullanıcı")]
         public int UserId { get; set; }
+
+        [DisplayName("Tarih")]
         public DateTime TaskDate { get; set; }
+
+        [DisplayName("Görev İsmi")]
         public string TaskName { get; set; }
+
+        [DisplayName("Görev açıklaması")]
         public string TaskDescription { get; set; }
 
+        [DisplayName("Kullanıcı")]
         public virtual User User { get; set; }
     }
 }
--- src/ToDoProject.Web/Models/User.cs
+++ src/ToDoProject.Web/Models/User.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 
 #nullable disable
 
@@ -13,9 +14,17 @@ namespace ToDoProject.Web.Models
         }
 
         public int Id { get; set; }
+
+        [DisplayName("Kullanıcı Adı")]
         public string UserName { get; set; }
+
+        [DisplayName("Kullanıcı Şifresi")]
         public string UserPassword { get; set; }
+
+        [DisplayName("İsim")]
         public string Name { get; set; }
+
+        [DisplayName("Soyisim")]
         public string Surname { get; set; }
 
         public virtual ICollection<Task> Tasks { get; set; }
