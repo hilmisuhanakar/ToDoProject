d4d04406f5eafaa158056e4e6948d83089a0c084
Hilmi Sühan Akar <hilmisuhanakar@outlook.com.tr>
2021-10-22 20:40:12 +0300

    Added EntityFramework
    EntityFramework Comfigured

---
+++ src/ToDoProject.Web/Models/ToDoDbContext.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+#nullable disable
+
+namespace ToDoProject.Web.Models
+{
+    public partial class ToDoDbContext : DbContext
+    {
+        public ToDoDbContext()
+        {
+        }
+
+        public ToDoDbContext(DbContextOptions<ToDoDbContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Task> Tasks { get; set; }
+        public virtual DbSet<User> Users { get; set; }
+
+        
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("Relational:Collation", "Turkish_CI_AS");
+
+            modelBuilder.Entity<Task>(entity =>
+            {
+                entity.Property(e => e.Id).HasColumnName("ID");
+
+                entity.Property(e => e.TaskDate).HasColumnType("date");
+
+                entity.Property(e => e.TaskDescription)
+                    .IsRequired()
+                    .HasMaxLength(50);
+
+                entity.Property(e => e.TaskName)
+                    .IsRequired()
+                    .HasMaxLength(50);
+
+                entity.Property(e => e.UserId).HasColumnName("UserID");
+
+                entity.HasOne(d => d.User)
+                    .WithMany(p => p.Tasks)
+                    .HasForeignKey(d => d.UserId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK_Tasks_Users");
+            });
+
+            modelBuilder.Entity<User>(entity =>
+            {
+                entity.Property(e => e.Id).HasColumnName("ID");
+
+                entity.Property(e => e.Name).HasMaxLength(50);
+
+                entity.Property(e => e.Surname).HasMaxLength(50);
+
+                entity.Property(e => e.UserName)
+                    .IsRequired()
+                    .HasMaxLength(50);
+
+                entity.Property(e => e.UserPassword)
+                    .IsRequired()
+                    .HasMaxLength(50);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
---
+++ src/ToDoProject.Web/Models/User.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace ToDoProject.Web.Models
+{
+    public partial class User
+    {
+        public User()
+        {
+            Tasks = new HashSet<Task>();
+        }
+
+        public int Id { get; set; }
+        public string UserName { get; set; }
+        public string UserPassword { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+
+        public virtual ICollection<Task> Tasks { get; set; }
+    }
+}
